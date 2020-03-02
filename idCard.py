a1407c3fbb58d177a2a052a55a43693d7d98f541
Pradeep Suthar <Sutharpradeep081@gmail.com>
2020-03-02 15:52:27 +0530

    Add files via upload

--- idCard.py
+++ idCard.py
@@ -5,13 +5,13 @@ def getCSVdata():
   df = pd.read_csv('identityCard.csv')
   for index, values in df.iterrows():
     roll_no = values['Roll No.']
-    name = values['Name']
+    name = values['Name ']
     father_name = values["Father's Name"]
     branch = values['Branch']
     dob = values['Date of Birth']
     address = values['Resi Address']
     mobile_no = values['Mobile No.']
-    batch = values['Batch No.']
+    batch = values['Betch No.']
 
     data = f'''
       Roll No. : {roll_no} \n
